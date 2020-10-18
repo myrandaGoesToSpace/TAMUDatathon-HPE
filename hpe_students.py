8e1f8a729b6834ccee5dd1e075be9eb1a0cf09f4
leerainwater <lee.rainwater@tamu.edu>
2020-10-18 10:40:51 -0500

    elr added footnote superscripts

--- hpe_students.py
+++ hpe_students.py
@@ -72,14 +72,14 @@ def main():
         st.write("* __Single-Parent Families__: encourage business partners to provide child care or transition parents to remote work")
         st.write("### HPE believes in investing in the community. Let's commit to our students and beat this pandemic together")
 
-        st.write("<small>Urban Institute. 2020. Household Conditions by Geographic School District. Accessible from https://datacatalog.urban.org/dataset/household-conditions-geographic-sc.... Data originally sourced from NHGIS, developed at the Urban Institute, and made available under the ODC-BY 1.0 Attribution License.</small>", unsafe_allow_html=True)
+        st.write("<small><sup>1</sup>Urban Institute. 2020. Household Conditions by Geographic School District. Accessible from https://datacatalog.urban.org/dataset/household-conditions-geographic-sc.... Data originally sourced from NHGIS, developed at the Urban Institute, and made available under the ODC-BY 1.0 Attribution License.</small>", unsafe_allow_html=True)
 #2 from pandey-tushar/main
     if nav == "Purpose":
         img = Image.open("covid_students_teacher.jpg")
         st.image(img, width=300)
         
         st.title("COVID-19 Impact on K-12 Students")
-        st.write("All around the world, K-12 students are adapting to the changes put in place by COVID-19. Schools are now conducted online, meaning that students are increasingly reliant on the resources available at home. Students at a significant disadvantage include those from low-income housholds, without access to home internet, with parents suffering from job instability, and in single-parent homes. HPE is committed to making a positive impact on the community and knows firsthand the advantage technology can provide. This data visualization tool uses a dataset from the Urban Institute to compare the percentages of students in each state living in poverty, without internet access, in homes with job instability, and living with a single parent. Each of these targeted communities needs extra support during the pandemic, and HPE has the resources to make a difference.")
+        st.write("All around the world, K-12 students are adapting to the changes put in place by COVID-19. Schools are now conducted online, meaning that students are increasingly reliant on the resources available at home. Students at a significant disadvantage include those from low-income housholds, without access to home internet, with parents suffering from job instability, and in single-parent homes. HPE is committed to making a positive impact on the community and knows firsthand the advantage technology can provide. This data visualization tool uses a dataset from the Urban Institute<sup>1</sup> to compare the percentages of students in each state living in poverty, without internet access, in homes with job instability, and living with a single parent. Each of these targeted communities needs extra support during the pandemic, and HPE has the resources to make a difference.", unsafe_allow_html=True)
         st.header("Why Students Matter to HPE")
         st.write("HPE knows that K-12 students are the next generation of engineers, programmers, creators, and scholars that will impact the world. That's why it's important to invest in these future community members and ensure their success.")
         st.header("Vulnerable Demographics")
